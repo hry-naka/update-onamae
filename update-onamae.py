ee0ca5cd4b18b37f8c52d964f62221d60f2e9174
Hiroyuki Nakamura <tel2sip@gmail.com>
2023-04-13 16:23:02 +0900

    bug fix

--- update-onamae.py
+++ update-onamae.py
@@ -39,17 +39,22 @@ def get_global_ip():
     return (requests.get(url).content.decode('utf-8'))
 
 
-def get_ns_record(domain):
+def get_a_record(host, domain):
     dig = shutil.which('dig')
     if dig is None:
         print('This tool need dig to be installed to executable path')
         print('Soryy, bye.')
         sys.exit(1)
     else:
-        ns_record = subprocess.Popen(
-            f'{dig} {domain} ns +short'.split(), stdout=subprocess.PIPE)
-        ns_record = ns_record.stdout.read().decode('utf-8').split()[0]
-        return ns_record
+        if host == '@':
+            a_record = subprocess.Popen(
+                f'{dig} {domain} a +short'.split(), stdout=subprocess.PIPE)
+        else:
+            a_record = subprocess.Popen(
+                f'{dig} {host}.{domain} a +short'.split(), stdout=subprocess.PIPE)
+        # a_record = a_record.stdout.read().decode('utf-8').split()[0]
+        a_record = a_record.stdout.read().decode('utf-8')
+        return a_record.strip()
 
 
 def read_config(filename):
@@ -82,17 +87,23 @@ def read_config(filename):
     if len(hostname) != len(ipv4):
         print('Wrong config file. number of hostname should be the same as the number of ipv4¥n')
         sys.exit(1)
-    return userid,password,domain,hostname,ipv4
+    return userid, password, domain, hostname, ipv4
 
 
-def convert_cmd(userid,password,domain,hostname,ipv4,global_ip):
+def convert_cmd(userid, password, domain, hostname, ipv4, global_ip):
     login_cmd = 'LOGIN¥n'
     login_cmd += f"USERID:{userid}¥n"
     login_cmd += f"PASSWORD:{password}¥n."
-    i=0
+    i = 0
     modify_cmd = ""
+    print( f"G-IP: {global_ip}:{type(global_ip)}")
     for host in hostname:
-        if ipv4[i] == global_ip:
+        if ipv4[i] == 'GLOBAL-IP':
+            ipv4[i] = global_ip
+        if ipv4[i] == get_a_record(host, domain):
+            print(
+                f"SKIP:{host}.{domain}'s ip address({ipv4[i]}) won't be changed.")
+            i += 1
             continue
         modify_cmd += 'MODIP¥n'
         if host == '@':
@@ -100,12 +111,8 @@ def convert_cmd(userid,password,domain,hostname,ipv4,global_ip):
         else:
             modify_cmd += f"HOSTNAME:{host}¥n"
         modify_cmd += f"DOMNAME:{domain}¥n"
-        if ipv4[i] == 'GLOBAL-IP':
-            modify_cmd += f"IPV4:{global_ip}¥n."
-        else:
-            modify_cmd += f"IPV4:{ipv4[i]}¥n."
+        modify_cmd += f"IPV4:{ipv4[i]}¥n."
         i += 1
-    print( f"number of host is {i}¥n")
     return login_cmd, modify_cmd
 
 
@@ -134,11 +141,12 @@ def send_logout():
     return
 
 
-def do_update(userid,password,domain,hostname,ipv4):
+def do_update(userid, password, domain, hostname, ipv4):
     global_ip = get_global_ip()  # global_ipを取得する
-    login_cmd, modify_cmd = convert_cmd(userid,password,domain,hostname,ipv4,global_ip)
-    print( login_cmd )
-    print( modify_cmd )
+    login_cmd, modify_cmd = convert_cmd(
+        userid, password, domain, hostname, ipv4, global_ip)
+    print(login_cmd)
+    print(modify_cmd)
     sys.exit(1)
     connect(get_ns_record(domain))  # domainのNSサーバにコネクト
     send_login(login_cmd)
@@ -147,7 +155,7 @@ def do_update(userid,password,domain,hostname,ipv4):
     return
 
 
-def daemonize(userid,password,domain,hostname,ipv4, interval):
+def daemonize(userid, password, domain, hostname, ipv4, interval):
     pid = os.fork()  # fork child process
     if pid > 0:  # Parrent proocess
         pid_file = open('/var/run/python_daemon.pid', 'w')
@@ -156,18 +164,20 @@ def daemonize(userid,password,domain,hostname,ipv4, interval):
         sys.exit()
     elif pid == 0:  # 子プロセスの場合
         while True:
-            do_update(userid,password,domain,hostname,ipv4)
+            do_update(userid, password, domain, hostname, ipv4)
             time.sleep(interval)
 
 
 if __name__ == '__main__':
     args = get_args()
 
-    userid,password,domain,hostname,ipv4 = read_config(args.filename[0])  # filenameを読み出す
-    if (int(args.interval) == 0):       
-        do_update(userid,password,domain,hostname,ipv4)
+    userid, password, domain, hostname, ipv4 = read_config(
+        args.filename[0])  # filenameを読み出す
+    if (int(args.interval) == 0):
+        do_update(userid, password, domain, hostname, ipv4)
     else:
-        daemonize(userid,password,domain,hostname,ipv4, convert_interval(args.interval))
+        daemonize(userid, password, domain, hostname,
+                  ipv4, convert_interval(args.interval))
     # print( args.filename )
     # print( args.interval )
     # print( get_global_ip() )
