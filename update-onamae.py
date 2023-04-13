ad45e90c79598e96f3e6ecbe4751036b4f9162ee
Hiroyuki Nakamura <tel2sip@gmail.com>
2023-04-13 15:09:22 +0900

    add functions for reading config file and convert to comand script. pls add some dummy function which is not yet to be implement.

--- update-onamae.py
+++ update-onamae.py
@@ -2,9 +2,9 @@
 # coding: utf-8
 import os
 import sys
+import time
 import shutil
 import subprocess
-import logging
 import socket
 import ssl
 import requests
@@ -15,24 +15,30 @@ def get_args():
     parser = argparse.ArgumentParser(
         description='update config file for onamae ddns service')
     parser.add_argument('-f', '--filename',
-                    metavar='config_filename',
-                    type=str,
-                    nargs=1,
-                    default='./.onamae-env',
-                    help='Set config filename')
+                        metavar='config_filename',
+                        type=str,
+                        nargs=1,
+                        default='./.onamae-env',
+                        help='Set config filename')
     parser.add_argument('-i', '--interval',
-                metavar='interval',
-                type=str,
-                nargs=1,
-                default="0",
-                help="Interval time(0(defalut):update only once, X:update every Xs, X[mh]: update every X[mh])." )
+                        metavar='interval',
+                        type=str,
+                        nargs=1,
+                        default="0",
+                        help="Interval time(0(defalut):update only once, X:update every Xs, X[mh]: update every X[mh]).")
     return parser.parse_args()
+
+
+def convert_interval(interval):
+    value = int(interval)  # Not yet to be implemented
+    return (value)
+
 
 def get_global_ip():
     url = 'https://ifconfig.me'
-    return( requests.get(url).content.decode('utf-8'))
+    return (requests.get(url).content.decode('utf-8'))
 
-    
+
 def get_ns_record(domain):
     dig = shutil.which('dig')
     if dig is None:
@@ -40,14 +46,128 @@ def get_ns_record(domain):
         print('Soryy, bye.')
         sys.exit(1)
     else:
-        ns_record = subprocess.Popen(f'{dig} {domain} ns +short'.split(), stdout=subprocess.PIPE)
+        ns_record = subprocess.Popen(
+            f'{dig} {domain} ns +short'.split(), stdout=subprocess.PIPE)
         ns_record = ns_record.stdout.read().decode('utf-8').split()[0]
         return ns_record
 
 
+def read_config(filename):
+    hostname = []
+    ipv4 = []
+    userid = password = domain = None
+    with open(filename) as f:
+        for l in f.readlines():
+            if len(l) == 0:
+                continue
+            k, v = l.rstrip().split('=')
+            if k == 'USERID':
+                userid = v
+            elif k == 'PASSWORD':
+                password = v
+            elif k == 'DOMNAME':
+                domain = v
+            elif k == 'HOSTNAME':
+                if len(v) == 0:
+                    v = '@'
+                hostname.append(v)
+            elif k == 'IPV4':
+                ipv4.append(v)
+    if userid is None or password is None or domain is None:
+        print("Wrong config file. userid,password,domain should be specified.¥n")
+        sys.exit(1)
+    if len(hostname) == 0 or len(ipv4) == 0:
+        print("Wrong config file. hostname, ipv4 should be specified.¥n")
+        sys.exit(1)
+    if len(hostname) != len(ipv4):
+        print('Wrong config file. number of hostname should be the same as the number of ipv4¥n')
+        sys.exit(1)
+    return userid,password,domain,hostname,ipv4
+
+
+def convert_cmd(userid,password,domain,hostname,ipv4,global_ip):
+    login_cmd = 'LOGIN¥n'
+    login_cmd += f"USERID:{userid}¥n"
+    login_cmd += f"PASSWORD:{password}¥n."
+    i=0
+    modify_cmd = ""
+    for host in hostname:
+        if ipv4[i] == global_ip:
+            continue
+        modify_cmd += 'MODIP¥n'
+        if host == '@':
+            modify_cmd += f"HOSTNAME:¥n"
+        else:
+            modify_cmd += f"HOSTNAME:{host}¥n"
+        modify_cmd += f"DOMNAME:{domain}¥n"
+        if ipv4[i] == 'GLOBAL-IP':
+            modify_cmd += f"IPV4:{global_ip}¥n."
+        else:
+            modify_cmd += f"IPV4:{ipv4[i]}¥n."
+        i += 1
+    print( f"number of host is {i}¥n")
+    return login_cmd, modify_cmd
+
+
+def connect():
+    # ドメインのNSレコードを取得する
+    # NSレコードのfqdnにsocketを接続する
+    return
+
+
+def send_login(login_cmd):
+    # 　login_cmdを送信する
+    # 　エラーだったら念のためLogoutする
+    return
+
+
+def send_modify(modify_cmd):
+    # modify_cmdを送信する
+    # 　エラーの場合はlogoutする
+    return
+
+
+def send_logout():
+    cmd = 'LOGOUT¥n.'
+    # cmdをsendする
+    # 　エラー処理する
+    return
+
+
+def do_update(userid,password,domain,hostname,ipv4):
+    global_ip = get_global_ip()  # global_ipを取得する
+    login_cmd, modify_cmd = convert_cmd(userid,password,domain,hostname,ipv4,global_ip)
+    print( login_cmd )
+    print( modify_cmd )
+    sys.exit(1)
+    connect(get_ns_record(domain))  # domainのNSサーバにコネクト
+    send_login(login_cmd)
+    send_modify(modify_cmd)
+    send_logout()
+    return
+
+
+def daemonize(userid,password,domain,hostname,ipv4, interval):
+    pid = os.fork()  # fork child process
+    if pid > 0:  # Parrent proocess
+        pid_file = open('/var/run/python_daemon.pid', 'w')
+        pid_file.write(str(pid)+"\n")
+        pid_file.close()
+        sys.exit()
+    elif pid == 0:  # 子プロセスの場合
+        while True:
+            do_update(userid,password,domain,hostname,ipv4)
+            time.sleep(interval)
+
+
 if __name__ == '__main__':
     args = get_args()
-    print( args.filename )
-    print( args.interval )
-    print( get_global_ip() )
- 
+
+    userid,password,domain,hostname,ipv4 = read_config(args.filename[0])  # filenameを読み出す
+    if (int(args.interval) == 0):       
+        do_update(userid,password,domain,hostname,ipv4)
+    else:
+        daemonize(userid,password,domain,hostname,ipv4, convert_interval(args.interval))
+    # print( args.filename )
+    # print( args.interval )
+    # print( get_global_ip() )
